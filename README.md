c5201872788a38e1dd7a0a5a14a158575d5f461e
atlan-antillia <atlan@antillia.com>
2024-02-02 18:16:37 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# Cervical-Cell-Nucleus-ImageDataset
-Cervical Cell Nucleus ImageMaskDataset for Image Segmentation
+<h2>Cervical-Cell-Nucleus-Image-Dataset (2024/02/02)</h2>
+The dataset used here has been take from the following <b>mendeley.com</b> website
+
+<a href="https://data.mendeley.com/datasets/jks43dkjj7/1">Data for: Binary Tree-like Network with Two-path Fusion Attention Feature for Cervical Cell Nucleus Segmentation
+</a>
+
+
+
+<h3>1. Dataset Citation</h3>
+
+<b>Data for: Binary Tree-like Network with Two-path Fusion Attention Feature for Cervical Cell Nucleus Segmentation</b>
+<pre>
+Published: 18 April 2019 Version 1
+
+https://data.mendeley.com/datasets/jks43dkjj7/1
+
+DOI:
+10.17632/jks43dkjj7.1
+
+Contributor:
+Jianwei Zhang
+
+Description
+The images are based on Liquid-Based Cytology test from the pathology departments of a Chinese hospital 
+and a biomedical device company. Under the guidance of a professional pathologist, we choose qualified 
+images in numerous LCT cervical images. The dataset contains 104 images with size  and each image has 
+a ground truth that has been manually segmented by a professional pathologist.
+
+Licence
+CC BY 4.0
+</pre>
+
+
+
+<h3>
+2. Download master dataset
+</h3>
+
+Please donwload master dataset from the following <b>mendeley.com</b> website
+
+<a href="https://data.mendeley.com/datasets/jks43dkjj7/1">Data for: Binary Tree-like Network with Two-path Fusion Attention Feature for Cervical Cell Nucleus Segmentation
+</a>
+
+
+The dataset has the following folder structure.<br>
+<pre>
+./dataset_forsubmit
+├─GroundTruth
+├─groundtruth_mask
+└─imgbmp
+</pre>
+GrounsTruth and imgbmp folders contains 104 bmp files respectively<br>
+
+<h3>
+3. Create ImageMask Dataset
+</h3>
+<h3>
+3.1 Create base dataset
+</h3>
+Please run Python script <a href="./generator/ImageMaskDatasetGenerator.py">ImageMaskDatasetGenerator.py</a>.
+to create jpg image and mask files.<br>
+<pre>
+>python create_base_dataset_512x512.py
+</pre>
+This script performs the following image processings.<br>
+<pre>
+1 Create 512x512 square masks from the original img files in <b>imgbmp</b> folder. 
+
+2 Create 512x512 square images corresponding to the image files from the original bmp files 
+in <b>GroundTruth</b> folder
+</pre>
+The created <b>Cervical_Cell_Nucleus_Master</b> dataset has the following folder structure.<br>
+
+<pre>
+./Cervical_Cell_Nucleus_Master
+ ├─images
+ └─masks
+</pre>
+
+
+
+<h3>
+3.2 Split master dataset to test, train and valid 
+</h3>
+By using Python script <a href="./generator/split_master.py">split_master.py</a>,
+ we have finally created <b>Cervical-Cell-Nucleus-ImageMask-Dataset</b> dataset from the <b>Cervical_Cell_Nucleus_Master</b>.<br>
+<pre>
+./Cervical-Cell-Nucleus-ImageMask-Dataset
+├─test
+│  ├─images
+│  └─masks
+├─train
+│  ├─images
+│  └─masks
+└─valid
+    ├─images
+    └─masks
+</pre>
+
+<b>train/images samples:</b><br>
+<img src="./asset/sample_train_images.png" width="1024" height="auto">
+<br>
+<b>train/masks samples:</b><br>
+<img src="./asset/sample_train_masks.png"  width="1024" height="auto">
+<br>
+<br>
+
+<img src="_Cervical-Cell-Nucleus-ImageMask-Dataset_.png"><br>
+
+You can download this dataset from the google drive 
+<a href="https://drive.google.com/file/d/1hyxctv-trt36A72YXHGklo5GEUZOf0jR/view?usp=sharing">
+Cervical-Cell-Nucleus-ImageMask-Dataset.zip</a>
+
---
+++ generator/ImageMaskDatasetGenerator.py
@@ -0,0 +1,137 @@
+import os
+import sys
+import glob
+import shutil
+import traceback
+import numpy as np
+
+from PIL import Image, ImageOps
+
+     
+class MasterDataset:
+  def __init__(self, resize=512):
+    self.RESIZE = resize
+    self.threshold = 128
+    self.ROTATION = True
+    self.file_extension = ".bmp"
+
+  def augment(self, image, output_dir, filename):
+  
+    if self.ROTATION:
+      ANGLES = [30, 90, 120, 150, 180, 210, 240, 270, 300, 330]
+
+      for angle in ANGLES:
+        rotated_image = image.rotate(angle)
+        output_filename = "rotated_" + str(angle) + "_" + filename
+        rotated_image_file = os.path.join(output_dir, output_filename)
+        #cropped  =  self.crop_image(rotated_image)
+        rotated_image.save(rotated_image_file)
+        print("=== Saved {}".format(rotated_image_file))
+      
+    # Create mirrored image
+    mirrored = ImageOps.mirror(image)
+    output_filename = "mirrored_" + filename
+    image_filepath = os.path.join(output_dir, output_filename)
+    #cropped = self.crop_image(mirrored)
+    
+    mirrored.save(image_filepath)
+    print("=== Saved {}".format(image_filepath))
+        
+    # Create flipped image
+    flipped = ImageOps.flip(image)
+    output_filename = "flipped_" + filename
+
+    image_filepath = os.path.join(output_dir, output_filename)
+    #cropped = self.crop_image(flipped)
+
+    flipped.save(image_filepath)
+    print("=== Saved {}".format(image_filepath))
+
+  def create(self, image_dir, mask_dir, output_dir):
+    self.create_resized_image(image_dir, output_dir, color=(255, 255, 255))
+    self.create_resized_mask(mask_dir, output_dir,   color=(255, 255, 255))
+
+
+  def create_resized_image(self, image_dir, output_dir, color=(255, 255, 255)):
+    output_image_dir = os.path.join(output_dir, "images")
+
+    if not os.path.exists(output_image_dir):
+       os.makedirs(output_image_dir)
+
+    image_files = glob.glob(image_dir + "/*.bmp")
+    for image_file in image_files:
+      image = Image.open(image_file)
+      resized_image = self.resize_to_square(image, color)
+      basename = os.path.basename(image_file)
+      name     = basename.split(".")[0]
+      filename = name + ".jpg"
+      output_filepath = os.path.join(output_image_dir, filename)
+      resized_image.save(output_filepath)
+      print("=== Saved {}".format(output_filepath))
+      
+      self.augment(resized_image, output_image_dir, filename)
+
+
+  def create_resized_mask(self, mask_dir, output_dir, color=(0, 0, 0)):
+    output_mask_dir = os.path.join(output_dir, "masks")
+    if not os.path.exists(output_mask_dir):
+      os.makedirs(output_mask_dir)
+
+    image_files = glob.glob(mask_dir + "/*.bmp")
+
+    for image_file in image_files:
+  
+      image = Image.open(image_file)
+      resized_image = self.resize_to_square(image, color)
+      resized_image = resized_image.convert("L")
+      resized_image = ImageOps.invert(resized_image)
+      basename = os.path.basename(image_file)
+      mask_pil = self.binarize(resized_image)
+  
+      name     = basename.split(".")[0]
+      filename = name + ".jpg"
+
+      output_filepath = os.path.join(output_mask_dir, filename)
+ 
+      mask_pil.save(output_filepath)
+      print("=== Saved {}".format(output_filepath))
+      self.augment(mask_pil, output_mask_dir, filename)
+
+
+  def resize_to_square(self, image, color):
+     w, h  = image.size
+
+     bigger = w
+     if h > bigger:
+       bigger = h
+     print("---background {}".format(color))
+     background = Image.new("RGB", (bigger, bigger), color)
+    
+     x = (bigger - w) // 2
+     y = (bigger - h) // 2
+     background.paste(image, (x, y))
+     background = background.resize((self.RESIZE, self.RESIZE))
+
+     return background
+  
+  def binarize(self, resized_image):
+    mask = np.array(resized_image)
+    mask[mask< self.threshold] =   0
+    mask[mask>=self.threshold] = 255
+    mask_pil = Image.fromarray(mask)
+    return mask_pil
+
+if __name__ == "__main__":
+  try:
+     image_dir = "./dataset_forsubmit/imgbmp/"
+     mask_dir  = "./dataset_forsubmit/GroundTruth/"
+     output_dir = "./Cervical_Cell_Nucleus_Master"
+     if os.path.exists(output_dir):
+       shutil.rmtree(output_dir)
+     if not os.path.exists(output_dir):
+       os.makedirs(output_dir)
+     dataset = MasterDataset()
+     dataset.create(image_dir, mask_dir, output_dir)
+
+  except:
+    traceback.print_exc()
